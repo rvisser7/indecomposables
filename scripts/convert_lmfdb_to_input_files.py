d99db2e35e06f3b631c5129b0d0aa757d19aac21
Robin Visser <robinvisser777@gmail.com>
2026-04-02 23:40:33 +0200

    Update LMFDB conversion script

--- scripts/convert_lmfdb_to_input_files.py
+++ scripts/convert_lmfdb_to_input_files.py
@@ -2,25 +2,32 @@
 # i.e. degN.txt should just be lmfdb_index|coeffs|monogenic, in a compact size (to save on filespace)
 
 # Convert from boolean to smallint (for monogenic)
-monogenic_dict = {"True" : 1, "False" : -1, "None" : 0}
+monogenic_dict = {True : 1, False : -1, None : 0}
 
-f_input = open("lmfdb_n_fields_0402_2007.sage", 'r')
-f_output = open("degN.txt", 'w')
+f_input = open("lmfdb_nf_fields_0402_2132.sage", 'r')
+f_output = open("degN_temp_output.txt", 'w')
 f_output.write("lmfdb_index|coeffs|monogenic\n")
 
-for line in f:
-    if line[0] == '[":
-      data = eval(line)
+for line in f_input:
+    if line[0] == '[':
+        # Get line of data
+        data = line.strip()
+        if data[-1]==",":
+            data = data[:-1]
+        data = eval(data)
 
-      lmfdb_index = data[0].split('.')[-1]
-      coeffs = str(data[1][:-1]).replace(' ', '').replace('[', '').replace(']', '')
-      monogenic = str(monogenic_dict[data[2]])
+        # Get LMFDB index of label
+        lmfdb_index = data[0].split('.')[-1]
 
-      output_line = lmfdb_index+|+coeffs+|+monogenic+"\n"
-      f_output.write(output_line)
+        # Get polynomial coeffs
+        assert(data[1][-1] == 1)
+        coeffs = str(data[1][:-1]).replace(' ', '').replace('[', '').replace(']', '')
+
+        # Get whether field is monogenic
+        monogenic = str(monogenic_dict[data[2]])
+
+        output_line = lmfdb_index+'|'+coeffs+'|'+monogenic+"\n"
+        f_output.write(output_line)
 
 f_input.close()
 f_output.close()
-
-
-  
