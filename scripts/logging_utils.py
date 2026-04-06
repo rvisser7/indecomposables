0f38ede6a084fea812abfa053d1e6f6c71145235
Robin Visser <robinvisser777@gmail.com>
2026-04-06 22:06:21 +0000

    Update logging behaviour for both console and log files (default: info for log files, and warnings for console)

--- scripts/logging_utils.py
+++ scripts/logging_utils.py
@@ -1,6 +1,23 @@
 import logging
 
 def setup_logger(name="indecomposables", log_file=None, verbose=False, debug=False):
+    """
+    Set up a logger with separate formatters for console and file output.
+    
+    Args:
+        name: Logger name (default: "indecomposables")
+        log_file: Path to log file (if None, no file logging)
+        verbose: If True, output INFO level messages to stdout
+        debug: If True, output DEBUG level messages to log file
+    
+    Behavior:
+        - Console output (stdout):
+          * Default: WARNING and above (no timestamp/levelname, message only)
+          * With --verbose: INFO and above (message only)
+        - File output (if log_file provided):
+          * Default: INFO and above (with timestamp/levelname)
+          * With --debug: DEBUG and above (with timestamp/levelname)
+    """
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
@@ -8,30 +25,40 @@ def setup_logger(name="indecomposables", log_file=None, verbose=False, debug=Fal
     if logger.hasHandlers():
         logger.handlers.clear()
 
-    # Formatter
-    formatter = logging.Formatter(
+    # Formatter for console: message only (no timestamp/levelname)
+    console_formatter = logging.Formatter("%(message)s")
+
+    # Formatter for file: include timestamp and levelname
+    file_formatter = logging.Formatter(
         "%(asctime)s | %(levelname)s | %(message)s",
         datefmt="%H:%M:%S"
     )
 
     # Console handler
     ch = logging.StreamHandler()
-    if debug:
-        console_level = logging.DEBUG
-    elif verbose:
+    
+    # Determine console level based on verbose flag
+    if verbose:
         console_level = logging.INFO
     else:
         console_level = logging.WARNING
 
     ch.setLevel(console_level)
-    ch.setFormatter(formatter)
+    ch.setFormatter(console_formatter)
     logger.addHandler(ch)
 
     # File handler
     if log_file:
         fh = logging.FileHandler(log_file)
-        fh.setLevel(logging.DEBUG)
-        fh.setFormatter(formatter)
+        
+        # Determine file level based on debug flag
+        if debug:
+            file_level = logging.DEBUG
+        else:
+            file_level = logging.INFO
+        
+        fh.setLevel(file_level)
+        fh.setFormatter(file_formatter)
         logger.addHandler(fh)
 
     return logger
